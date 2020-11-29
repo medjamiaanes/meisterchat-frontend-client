bc1a6ad109b7f50d8459bff3456d65282aacbf07
medjamiaanes <medjamia.anes@gmail.com>
2020-11-29 22:11:33 +0100

    Removed counter reducer/slice from the redux store & added the user reducer/slice

---
+++ src/features/userSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: null,
+  reducers: {
+    login: (state, action) => action.payload,
+    logout: (state) => null,
+  },
+})
+
+export const { login, logout } = userSlice.actions
+
+export default userSlice.reducer
