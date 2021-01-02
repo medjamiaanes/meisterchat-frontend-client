3a801e808e94695a29b005f5c185f323617d945d
medjamiaanes <medjamia.anes@gmail.com>
2021-01-02 21:16:19 +0100

    -Added avatar & default avatar for the chat item -Changed online status to active -Added offline status background color

--- src/pages/Main/views/Chat/components/ChatItem/ChatItem.js
+++ src/pages/Main/views/Chat/components/ChatItem/ChatItem.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './chat_item.scss'
 import { AccessTime } from '@material-ui/icons'
+import DefaultAvatar from '../../../../../../images/default_avatar.png'
 const ChatItem = ({ status, avatar, username, message, active, onClick }) => {
   const renderStatus = () => {}
   return (
     <div className={`chat_item ${active ? 'active' : ''}`} onClick={onClick}>
       <div className="avatar">
-        <img src={avatar} alt="user_avatar" className="user_img" />
+        <img
+          src={avatar || DefaultAvatar}
+          alt="user_avatar"
+          className="user_img"
+        />
         <div className={`status ${status}`}></div>
       </div>
       <div className="chat_body">
