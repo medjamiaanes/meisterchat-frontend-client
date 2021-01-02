dded57719e44796bacb157897e6a9c8b4cbf0a4e
medjamiaanes <medjamia.anes@gmail.com>
2021-01-02 21:05:40 +0100

    Added the ComingSoon page

---
+++ src/pages/Main/views/ComingSoon/ComingSoon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Inc from '../../../../images/inc.jpg'
+import './coming_soon.scss'
+const ComingSoon = () => {
+  return (
+    <div className="coming_soon">
+      <img src={Inc} alt="Comming Soon" />
+    </div>
+  )
+}
+
+export default ComingSoon
