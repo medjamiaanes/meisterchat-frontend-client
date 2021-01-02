a6abb39de2f0d601e811d1c8987fc1b84ef5c230
medjamiaanes <medjamia.anes@gmail.com>
2021-01-02 21:14:22 +0100

    Enhanced sidebar : - Enhanced styling for user avatar - Added links & active_link class

--- src/pages/Main/components/Sidebar/Sidebar.js
+++ src/pages/Main/components/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import DefaultAvatar from '../../../../images/chandler.jpeg'
+import DefaultAvatar from '../../../../images/default_avatar.png'
 import './sidebar.scss'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink, Link, useRouteMatch } from 'react-router-dom'
 import { Modal, Button } from 'rsuite'
 import {
   Sms,
@@ -11,30 +11,52 @@ import {
   Settings,
   ExitToApp,
 } from '@material-ui/icons'
+import { useSelector } from 'react-redux'
 const Sidebar = () => {
   const [modal, setModal] = React.useState(false)
+  const user = useSelector((state) => state.user.user)
+  const match = useRouteMatch()
   const submitLogout = () => {
     localStorage.removeItem('meisterchat_auth')
     window.location.replace('/')
   }
+  React.useEffect(() => {
+    console.log(user)
+  }, [])
   return (
     <div className="sidebar">
       <div className="sidebar_header">
         <div className="avatar">
-          <img src={DefaultAvatar} alt="user_avatar" />
+          <img src={user.avatar || DefaultAvatar} alt="user_avatar" />
         </div>
       </div>
       <div className="sidebar_body">
-        <NavLink to="" className="sidebar_link">
+        <NavLink
+          to="/chat"
+          className="sidebar_link"
+          activeClassName="active_link"
+        >
           <Sms className="link_icon" />
         </NavLink>
-        <NavLink to="" className="sidebar_link">
+        <NavLink
+          to="/calls"
+          className="sidebar_link"
+          activeClassName="active_link"
+        >
           <Call className="link_icon" />
         </NavLink>
-        <NavLink to="" className="sidebar_link">
+        <NavLink
+          to="/profile"
+          className="sidebar_link"
+          activeClassName="active_link"
+        >
           <Person className="link_icon" />
         </NavLink>
-        <NavLink to="" className="sidebar_link">
+        <NavLink
+          to="/notifications"
+          className="sidebar_link"
+          activeClassName="active_link"
+        >
           <Notifications className="link_icon" />
         </NavLink>
       </div>
@@ -47,7 +69,12 @@ const Sidebar = () => {
         </Link>
       </div>
 
-      <Modal keyboard={false} show={modal} onHide={() => setModal(false)}>
+      <Modal
+        backdrop="static"
+        keyboard={false}
+        show={modal}
+        onHide={() => setModal(false)}
+      >
         <Modal.Header className="modal_header">
           <ExitToApp className="modal_icon" />{' '}
           <h5 className="modal_title">Logging out</h5>
