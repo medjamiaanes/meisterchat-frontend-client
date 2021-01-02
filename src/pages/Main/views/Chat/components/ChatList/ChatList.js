73249b33a0228e92c3ddda5c166aa346116fabd5
medjamiaanes <medjamia.anes@gmail.com>
2021-01-02 21:17:09 +0100

    Enhanced chat list - Linked chat list with the store (chat slice) - Enhanced styling & design

--- src/pages/Main/views/Chat/components/ChatList/ChatList.js
+++ src/pages/Main/views/Chat/components/ChatList/ChatList.js
@@ -1,70 +1,90 @@
 import React from 'react'
 import './chat_list.scss'
-import { Search } from '@material-ui/icons'
+import { Search, Sms } from '@material-ui/icons'
 import ChatItem from '../ChatItem'
-import Chandler from '../../../../../../images/chandler.jpeg'
-import Monica from '../../../../../../images/monica.jpg'
+import AddChat from '../AddChat/'
+import { useSelector, useDispatch } from 'react-redux'
+import { setChat } from '../../../../../../features/chatSlice'
+import NoItems from '../NoItems'
 const ChatList = () => {
-  const chatList = [
-    {
-      id: 1,
-      username: 'Monica Geller',
-      message: "Hi i'm monica and i'm a chef",
-      status: 'online',
-      active: true,
-      avatar: Monica,
-    },
-    {
-      id: 2,
-      username: 'chandler bing',
-      message: "Hi i'm chandler and i use humor as a defense mechanism",
-      status: 'offline',
-      avatar: Chandler,
-    },
-    {
-      id: 3,
-      username: 'chandler bing',
-      message: "Hi i'm chandler and i use humor as a defense mechanism",
-      status: 'busy',
-      avatar: Chandler,
-    },
-    {
-      id: 4,
-      username: 'chandler bing',
-      message: "Hi i'm chandler and i use humor as a defense mechanism",
-      status: 'absent',
-      avatar: Chandler,
-    },
-  ]
-  const [chats, setChats] = React.useState(chatList)
-  const [chat, setChat] = React.useState(undefined)
+  const dispatch = useDispatch()
+  const chatList = useSelector((state) => state.chat.chatList)
+  const chat = useSelector((state) => state.chat.currentChat)
+  const [search, setSearch] = React.useState('')
 
+  const renderList = () => {
+    if (search.trim()) return renderFilteredList()
+    return renderAllList()
+  }
+  const renderFilteredList = () => {
+    const list = chatList.filter(
+      (chat) =>
+        chat.user.username.includes(search) || chat.user.phone.includes(search),
+    )
+    return list.length ? (
+      list.map((c, index) => {
+        return (
+          <>
+            <ChatItem
+              status={c.user.isOnline ? c.user.status : 'offline'}
+              avatar={c.user.avatar}
+              username={c.user.username}
+              message={
+                c.messages.length ? c.messages[0].textMessage : 'New disscusion'
+              }
+              active={chat && c.user._id === chat.user._id}
+              key={index}
+              onClick={() => dispatch(setChat(c))}
+            />
+            <div className="divider"></div>
+          </>
+        )
+      })
+    ) : (
+      <NoItems icon={Sms} description="No disscusions found" />
+    )
+  }
+  const renderAllList = () => {
+    return chatList.length ? (
+      chatList.map((c, index) => {
+        return (
+          <>
+            <ChatItem
+              status={c.user.isOnline ? c.user.status : 'offline'}
+              avatar={c.user.avatar}
+              username={c.user.username}
+              message={
+                c.messages.length
+                  ? c.messages[0].textMessage
+                  : 'Click to start disscustion'
+              }
+              active={chat && c.user._id === chat.user._id}
+              key={index}
+              onClick={() => dispatch(setChat(c))}
+            />
+            <div className="divider"></div>
+          </>
+        )
+      })
+    ) : (
+      <NoItems icon={Sms} description="No disscusions found" />
+    )
+  }
   return (
     <div className="chat_list">
       <div className="header">
         <div className="search">
           <Search className="search_icon" />
-          <input type="text" className="search_input" placeholder="Search" />
+          <input
+            type="text"
+            className="search_input"
+            placeholder="Search"
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
       </div>
-      <div className="chat_items">
-        {chats.map((c, index) => {
-          return (
-            <>
-              <ChatItem
-                status={c.status}
-                avatar={c.avatar}
-                username={c.username}
-                message={c.message}
-                active={chat && c.id === chat.id}
-                key={index}
-                onClick={() => setChat(c)}
-              />
-              <div className="divider"></div>
-            </>
-          )
-        })}
-      </div>
+      <AddChat />
+      <div className="chat_items">{renderList()}</div>
     </div>
   )
 }
