aba562d86531a45ae2c4d58efebff8de207e6e1f
medjamiaanes <medjamia.anes@gmail.com>
2021-01-02 21:17:59 +0100

    Added fetchChats method to fetch chat list from the backend & added conditional rendering for the chat window

--- src/pages/Main/views/Chat/Chat.js
+++ src/pages/Main/views/Chat/Chat.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import ChatList from './components/ChatList'
 import ChatWindow from './components/ChatWindow'
+import StartChat from './components/StartChat'
 import './chat.scss'
+import { useSelector, useDispatch } from 'react-redux'
+import { setChatList } from '../../../../features/chatSlice'
+import axios from '../../../../helpers/Api'
+import { Alert } from 'rsuite'
 const Chat = () => {
+  const chat = useSelector((state) => state.chat.currentChat)
+  const dispatch = useDispatch()
+  const fetchChats = async () => {
+    try {
+      const { data } = await axios.get('/backend/api/inbox/fetch')
+      console.log(data)
+      dispatch(setChatList(data.chats))
+    } catch (error) {
+      Alert.error('500 Error code')
+    }
+  }
+  React.useEffect(() => {
+    fetchChats()
+  }, [])
   return (
     <div className="chat_page">
       <ChatList />
-      <ChatWindow />
+      {chat ? <ChatWindow /> : <StartChat />}
     </div>
   )
 }
